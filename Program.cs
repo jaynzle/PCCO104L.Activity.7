355b7679401599f8476e8dd25485f118869bead9
nathan <158656853+jaynzle@users.noreply.github.com>
2024-05-31 10:45:05 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,94 +1,91 @@
 using System;
 
-namespace Program
+public class Program
 {
-    internal class Program
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Char RickGrimes = new Char(1025, "Rick Grimes", "Revolver", 45, "Leader", true);
-            RickGrimes.Intro();
+        Char RickGrimes = new Char(1025, "Rick Grimes", "Revolver", 42, "Leader", true);
+        RickGrimes.Intro();
 
-            Char DarylDixon = new Char(987, "Daryl Dixon", "Crossbow", 30, "Hunter", true);
-            DarylDixon.Intro();
+        Char DarylDixon = new Char(987, "Daryl Dixon", "Crossbow", 45, "Hunter", true);
+        DarylDixon.Intro();
 
-            Char GlennRhee = new Char(752, "Glenn Rhee", "Butterfly Knife", 25, "Scout", true);
-            GlennRhee.Intro();
-        }
-    }
-
-    public class Char
-    {
-        private string Name { get; set; }
-        private string Weapon { get; set; }
-        private double Kills { get; set; }
-
-        public int Age { get; set; }
-        public string Role { get; set; }
-        public bool IsAlive { get; set; }
-
-        public Char() 
-        {
-            Name = "Unknown";
-            Weapon = "Unknown";
-            Kills = 0;
-            Age = 0;
-            Role = "Unknown";
-            IsAlive = false;
-        }
-
-        public Char(double kills)
-        {
-            Kills = kills;
-            Name = "Unknown";
-            Weapon = "Unknown";
-            Age = 0;
-            Role = "Unknown";
-            IsAlive = false;
-        }
-
-        public Char(double kills, string name) : this(kills)
-        {
-            Name = name;
-            Weapon = "Unknown";
-            Age = 0;
-            Role = "Unknown";
-            IsAlive = false;
-        }
-
-        public Char(double kills, string name, string weapon) : this(kills, name)
-        {
-            Weapon = weapon;
-            Age = 0;
-            Role = "Unknown";
-            IsAlive = false;
-        }
-
-        public Char(double kills, string name, string weapon, int age, string role, bool isAlive) : this(kills, name, weapon)
-        {
-            Age = age;
-            Role = role;
-            IsAlive = isAlive;
-        }
-
-        public void Intro()
-        {
-            Console.WriteLine($"{Name} - {Weapon} - {Kills} kills. Age: {Age}, Role: {Role}, Alive: {IsAlive}");
-        }
-
-        private double CalculateEfficiency()
-        {
-            return Kills / (Age > 0 ? Age : 1);
-        }
-
-        public double GetEfficiency()
-        {
-            return CalculateEfficiency();
-        }
-
-        public void ChangeWeapon(string newWeapon)
-        {
-            Weapon = newWeapon;
-        }
+        Char GlennRhee = new Char(752, "Glenn Rhee", "Butterfly Knife", 25, "Scout", false);
+        GlennRhee.Intro();
     }
 }
+
+public class Char
+{
+    private string Name { get; set; }
+    private string Weapon { get; set; }
+    private double Kills { get; set; }
+
+    public int Age { get; set; }
+    public string Role { get; set; }
+    public bool IsAlive { get; set; }
+
+    public Char() 
+    {
+        Name = "Unknown";
+        Weapon = "Unknown";
+        Kills = 0;
+        Age = 0;
+        Role = "Unknown";
+        IsAlive = false;
+    }
+
+    public Char(double kills)
+    {
+        Kills = kills;
+        Name = "Unknown";
+        Weapon = "Unknown";
+        Age = 0;
+        Role = "Unknown";
+        IsAlive = false;
+    }
+
+    public Char(double kills, string name) : this(kills)
+    {
+        Name = name;
+        Weapon = "Unknown";
+        Age = 0;
+        Role = "Unknown";
+        IsAlive = false;
+    }
+
+    public Char(double kills, string name, string weapon) : this(kills, name)
+    {
+        Weapon = weapon;
+        Age = 0;
+        Role = "Unknown";
+        IsAlive = false;
+    }
+
+    public Char(double kills, string name, string weapon, int age, string role, bool isAlive) : this(kills, name, weapon)
+    {
+        Age = age;
+        Role = role;
+        IsAlive = isAlive;
+    }
+
+    public void Intro()
+    {
+        Console.WriteLine($"{Name} - {Weapon} - {Kills} kills. Age: {Age}, Role: {Role}, Alive: {IsAlive}");
+    }
+
+    private double CalculateEfficiency()
+    {
+        return Kills / (Age > 0 ? Age : 1);
+    }
+
+    public double GetEfficiency()
+    {
+        return CalculateEfficiency();
+    }
+
+    public void ChangeWeapon(string newWeapon)
+    {
+        Weapon = newWeapon;
+    }
+    }
